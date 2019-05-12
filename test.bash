a545a5bda1cc4891bd38029dc71776bb4205822f
Adrien Ferrand <adrien.ferrand@eff.org>
2019-05-12 23:46:25 +0200

    Update tests

--- test.bash
+++ test.bash
@@ -1,34 +1,13 @@
 #!/bin/bash
-
 set -ex
 
-apt-get -y install lsb-release net-tools wget python nginx
+git clone https://github.com/certbot/certbot.git --single-branch --depth=1
+cd certbot
+./certbot-auto -n --os-packages-only
 
-wget https://github.com/docker/compose/releases/download/1.15.0-rc1/docker-compose-Linux-x86_64 -O /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+python3 -m venv venv
+. venv/bin/activate
 
-cat << EOF > /usr/local/bin/coverage
-#!/bin/bash -xe
+./tools/pip_install_editable.py certbot-ci
 
-if [ "\$1" != "run" ]; then
-    exit 0;
-fi
-
-"\${@:7}"
-EOF
-chmod +x /usr/local/bin/coverage
-
-certbot_version=$(certbot --version 2>&1 | grep "^certbot" | cut -d " " -f 2)
-
-cd parts/certbot/src
-
-tests/boulder-fetch.sh
-until curl http://localhost:4000/directory 2>/dev/null; do
-  echo waiting for boulder
-  sleep 1
-done
-# Not needed under Travis Trusty?
-#sed -i "s/'1.3.6.1.5.5.7.1.24'/-e '1.3.6.1.5.5.7.1.24' -e 'status_request'/g" tests/certbot-boulder-integration.sh
-tests/boulder-integration.sh
-
-echo "Success!"
+pytest certbot-ci/certbot_integration_tests -numprocesses 4 --acme-server=pebble 
