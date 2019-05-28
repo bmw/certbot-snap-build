23e02cd09c8fd3e0571638959ac2498d599ac214
Adrien Ferrand <ferrand.ad@gmail.com>
2019-05-28 16:01:26 +0200

    Clean tests

--- test.sh
+++ test.sh
@@ -8,4 +8,4 @@ python3 -m venv venv
 . venv/bin/activate
 pip install -e certbot-ci
 
-pytest certbot-ci/certbot_integration_tests --numprocesses 4 --acme-server=pebble 
+pytest certbot-ci/certbot_integration_tests/certbot_tests --numprocesses 4 --acme-server=pebble 
