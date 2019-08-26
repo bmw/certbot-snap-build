1d58e166c142b65cd39180484d6b95d5377a753f
Adrien Ferrand <ferrand.ad@gmail.com>
2019-08-26 22:47:45 +0200

    Config tests

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 set -ex
 
 python3 -m venv venv
-venv/bin/python -m pip install -e certbot-ci
-venv/bin/python -m pytest certbot/certbot-ci/certbot_integration_tests --numprocesses 4 --acme-server=pebble 
+venv/bin/python -m pip install -e certbot/certbot-ci
+venv/bin/python -m pytest certbot/certbot-ci/certbot_integration_tests -n 4
