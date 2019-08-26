e9fee900645e4ce389ee5542315d155ebcfde90a
Adrien Ferrand <adferrand@users.noreply.github.com>
2019-08-26 22:22:43 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ set -ex
 
 python3 -m venv certbot/venv
 certbot/venv/bin/pip install -e certbot-ci
-certbot/venv/bin/pytest certbot-ci/certbot_integration_tests --numprocesses 4 --acme-server=pebble 
+certbot/venv/bin/pytest certbot/certbot-ci/certbot_integration_tests --numprocesses 4 --acme-server=pebble 
