ff53e021a5030ff69e88bf6743a2b8db3db09deb
Adrien Ferrand <adrien.ferrand@eff.org>
2019-08-30 01:49:47 +0200

    Add an apache test

--- test.sh
+++ test.sh
@@ -4,3 +4,11 @@ set -ex
 python3 -m venv venv
 venv/bin/python -m pip install -e certbot/certbot-ci
 venv/bin/python -m pytest certbot/certbot-ci/certbot_integration_tests -n 4
+
+# DO NOT RUN `apache-conf-test` LOCALLY, IT WILL BREAK YOUR APACHE CONFIGURATION!
+if [ -n "$TRAVIS" ]; then
+    if ! certbot/certbot-apache/certbot_apache/tests/apache-conf-files/apache-conf-test --debian-modules; then
+    echo "Apache tests failed!" >&2
+    exit 1
+    fi
+fi
