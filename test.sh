0e26e48269901ba79f3083ad422071d800cc3173
Adrien Ferrand <ferrand.ad@gmail.com>
2019-08-30 16:30:56 +0200

    Use virtual environment

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ venv/bin/python -m pytest certbot/certbot-ci/certbot_integration_tests -n 4
 
 # DO NOT RUN `apache-conf-test` LOCALLY, IT WILL BREAK YOUR APACHE CONFIGURATION!
 if [ -n "$TRAVIS" ]; then
-    if ! certbot/certbot-apache/certbot_apache/tests/apache-conf-files/apache-conf-test-pebble.py --debian-modules; then
+    if ! venv/bin/python certbot/certbot-apache/certbot_apache/tests/apache-conf-files/apache-conf-test-pebble.py --debian-modules; then
     echo "Apache tests failed!" >&2
     exit 1
     fi
